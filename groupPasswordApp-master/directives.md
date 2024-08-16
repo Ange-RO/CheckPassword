6119226eef4668609b0103da1c05296f58144cb1
ange houndjo <houndjoangeplus@gmail.com>
2024-08-16 14:35:03 +0200

    chekPassword

---
+++ groupPasswordApp-master/directives.md
@@ -0,0 +1,15 @@
+-mise en place du composant form
+- mise en place du composant de verification 
+- mise en place du formulaire avec des [formControl]="";
+- mise en place du script du formulaire avec les new FormControl 
+- envoie du formcontrol du password au composant parent 
+- envoi du password du compant parent a la page de vérification 
+- conditionnement du style et de l'affichage des messages d'erreur en fonction du password validator. Par exemple si le required du password est validé :
+ password.errors?.['required'].
+ ......
+
+ rules de validation :  [Validators.required,
+        Validators.minLength(8),
+        Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/)]
+
+        
---
+++ groupPasswordApp-master/src/app/form/form.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html',
+  styleUrl: './form.component.css',
+})
+export class FormComponent {
+  showMessage: boolean = false;
+  currentEmail!: string;
+  currentPassword!: string;
+  submitPassword(password: string, email: string) {
+    localStorage.setItem('email', email);
+    localStorage.setItem('password', password);
+    this.currentEmail = localStorage.getItem('email') || '';
+    this.currentPassword = localStorage.getItem('password') || '';
+    this.showMessage = true;
+    console.log(localStorage.getItem('email'));
+  }
+
+  formPass = new FormGroup({
+    email: new FormControl('',[Validators.email]),
+    password: new FormControl('', [
+      Validators.pattern(
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d@$!%*?&]{8,}$/
+      ),
+    ]),
+  });
+}
+// Validators.minLength(8),Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/)
